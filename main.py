e2b9650a786209c0ae5a979b1506a65245b0a611
DrLokin <daniel.lanzav@gmail.com>
2024-03-20 17:30:49 -0400

    Creating the project basics

---
+++ main.py
@@ -0,0 +1,11 @@
+class NotifSender():
+
+    def main(self):
+        print("Hello world")
+        pass
+    pass
+pass
+
+if(__name__ == "__main__"):
+    app = NotifSender()
+    NotifSender.main(app)
